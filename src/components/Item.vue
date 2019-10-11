2ed8b86f06202bec43fc0273a13ebaefd6457f26
Anurak Sumranphan <a.sumranphan@gmail.com>
2019-10-11 11:37:35 +0700

    feat: split an item list displayed to the item components instead

---
+++ src/components/Item.vue
@@ -0,0 +1,55 @@
+<template>
+  <article class="stories-item">
+    <span class="item-score">{{ item.score }}</span>
+
+    <h5 class="item-title">
+      {{ item.title }}
+    </h5>
+
+    <div class="item-meta">
+      <span class="by">
+        by {{ item.by }}
+      </span>
+    </div>
+  </article>
+</template>
+
+<script>
+export default {
+  props: {
+    item: Object
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.stories-item {
+  position: relative;
+  padding: $story-item-padding-y $story-item-padding-x;
+  padding-left: $story-item-padding-x * 4;
+  border-bottom: 1px solid $story-item-border-color;
+
+  .item-score {
+    position: absolute;
+    color: $story-item-score-color;
+    font-size: $story-item-score-font-size;
+    font-weight: 700;
+    top: 50%;
+    left: 0;
+    width: 80px;
+    text-align: center;
+    transform: translateY(-50%);
+  }
+
+  .item-title {
+    font-size: $story-item-title-font-size;
+    line-height: 24px;
+    margin: 0;
+  }
+
+  .item-meta {
+    font-size: $story-item-meta-font-size;
+    color: $story-item-meta-color;
+  }
+}
+</style>
--- src/views/ListView.vue
+++ src/views/ListView.vue
@@ -2,23 +2,15 @@
   <div class="stories-view">
     <div class="stories-container">
       <div class="stories-list">
-        <article class="stories-item">
-          <span class="item-score">{{ item.score }}</span>
-          <h5 class="item-title">
-            {{ item.title }}
-          </h5>
-          <div class="item-meta">
-            <span class="posted-by">
-              by {{ item.by }}
-            </span>
-          </div>
-        </article>
+        <item :item="item"></item>
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import Item from '../components/Item.vue'
+
 export default {
   props: {
     type: String
@@ -36,7 +28,9 @@ export default {
         url: "https://techtalksatseavision.splashthat.com/"
       }
     }
-  }
+  },
+
+  components: { Item }
 }
 </script>
 
@@ -51,36 +45,6 @@ export default {
 
   .stories-list {
     background: $white;
-  }
-
-  .stories-item {
-    position: relative;
-    padding: $story-item-padding-y $story-item-padding-x;
-    padding-left: $story-item-padding-x * 4;
-    border-bottom: 1px solid $story-item-border-color;
-
-    .item-score {
-      position: absolute;
-      color: $story-item-score-color;
-      font-size: $story-item-score-font-size;
-      font-weight: 700;
-      top: 50%;
-      left: 0;
-      width: 80px;
-      text-align: center;
-      transform: translateY(-50%);
-    }
-
-    .item-title {
-      font-size: $story-item-title-font-size;
-      line-height: 24px;
-      margin: 0;
-    }
-
-    .item-meta {
-      font-size: $story-item-meta-font-size;
-      color: $story-item-meta-color;
-    }
   }
 }
 </style>
