42b0be79a634e0ca997499b9fa918256cc2b1d2d
Anurak Sumranphan <a.sumranphan@gmail.com>
2021-04-24 20:35:26 +0700

    chore: update a vue.config.js file

--- vue.config.js
+++ vue.config.js
@@ -1,13 +1,13 @@
 module.exports = {
-    css: {
-      loaderOptions: {
-        scss: {
-          data: `@import "@/variables.scss";`
-        }
+  css: {
+    loaderOptions: {
+      scss: {
+        additionalData: `@import "@/variables.scss";`
       }
-    },
-    pwa: {
-      themeColor: '#41b883',
-      msTileColor: '#41b883',
     }
+  },
+  pwa: {
+    themeColor: '#41b883',
+    msTileColor: '#41b883',
+  }
 }
